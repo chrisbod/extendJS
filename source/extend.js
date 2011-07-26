1deda37a1f45320f93ec2b8c5781576858a67da5
christianB <christian.bodart@thisisnumero.com>
2011-07-26 13:11:08 +0100

    Relinted, fixed potential problem with array.length and passing in floats as a filter

--- source/extend.js
+++ source/extend.js
@@ -1,9 +1,3 @@
-if (typeof this.extend !== "undefined") {
-	/*	a property called extend has already been declared in global scope!
-		store it and we will reassign later */
-	this.extend_placeholder = this.extend;
-}
-
 //core function
 this.extend = function extend(targetObject, sourceObject, filter) {
 	var propertyName,
@@ -27,33 +21,28 @@ this.extend = function extend(targetObject, sourceObject, filter) {
 		filterProperty = new arguments.callee.PropertyFilter(filter);
 	}
 	//enumerate, filter and assign
-	for (var propertyName in sourceObject) {
-		if (filterProperty.filterProperty(propertyName, targetObject, sourceObject)) {
-			targetObject[propertyName] = sourceObject[propertyName];
+	//Arrays should only be passed the length if we have specified null or false as a filter
+	if (typeof targetObject === Array && filter === false) {
+		for (propertyName in sourceObject) {
+			if (propertyName !== "length" && filterProperty.filterProperty(propertyName, targetObject, sourceObject)) {
+				targetObject[propertyName] = sourceObject[propertyName];	
+			}
+		}
+	} else {
+		for (propertyName in sourceObject) {
+			if (filterProperty.filterProperty(propertyName, targetObject, sourceObject)) {
+				targetObject[propertyName] = sourceObject[propertyName];
+			}
 		}
 	}
 };
 this.extend(
 	//self test 1 - passing null
-	this.extend, {
-		//housekeeping method to remove any references the named functions leave in global scope;
-		cleanReferences: function xtend_cleanReferences(scope) {
-			//cleans up any pollution caused by naming our functions by iterating and deleting the member
-			for (var propertyName in this.PropertyFilter.prototype) {
-				delete scope["extend_PropertyFilter_" + propertyName];
-			}
-			//if we have used a placeholder property, clean up
-			if (scope.extend_placeholder) {
-				scope.extend = extend_placeholder
-				delete scope.extend_placeholder;
-			}
-			//delete the constructor for the PropertyFilter from global scope
-			delete scope.extend_PropertyFilter;
-			//voila should be clean now!!!! (apart from xtend that is!)
-		},
+	this.extend, 
+	{
 		//extends core PropertyFilter object
 		PropertyFilter: function extend_PropertyFilter(filter) {
-			this.filter = filter||false;
+			this.filter = filter || false;
 			//decide the strategy for filtering
 			this.filterProperty = this.resolvePropertyFilterMethod();
 		}
@@ -63,14 +52,15 @@ this.extend(
 );
 this.extend(
 	//extend the prototype of PropertyFilter type
-	this.extend.PropertyFilter.prototype, {
+	this.extend.PropertyFilter.prototype, 
+	{
 	
 	//Core implementation for PropertyInterface Implementation//	
 		
 		//the only method 'required' to fulfill a FilterProperty 'implementation'
 		filterProperty: function extend_PropertyFilter_filterProperty() {
 			//defaut behaviour (occurs when no filter has been explicitly set in the object or the filter is set to false)
-			return this.filterPropertyInstanceOnlyAllowingNewNullOrUndefined.apply(this,arguments)
+			return this.filterPropertyInstanceOnlyAllowingNewNullOrUndefined.apply(this, arguments);
 		},
 		
 		//default behaviour is to only copy new properties or those in the target set to null or undefined
@@ -80,7 +70,7 @@ this.extend(
 		resolvePropertyFilterMethod: function extend_PropertyFilter_resolvePropertyFilterMethods() {
 			switch (this.filter.constructor) {
 				case Boolean	: return this.filter ? this.filterPropertyInstanceOnlyAllowingNewNullOrUndefined : this.filterPropertyAllPropertiesAllowingNewNullOrUndefined ;
-				case Number		: //do the same as number
+				case Number		: return this.filterPropertyUsingNumber;
 				case String		: return this.filterPropertyUsingString;
 				case Function	: return this.filterPropertyUsingFunction;
 				case RegExp		: return this.filterPropertyUsingRegExp;
@@ -94,7 +84,7 @@ this.extend(
 			return sourceObject.hasOwnProperty(propertyName) && (!targetObject.hasOwnProperty(propertyName) || targetObject[propertyName] === null || targetObject[propertyName] === undefined);
 		},
 
-		filterPropertyAllowingAll: function extend_PropertyFilter_filterPropertyAllPropertiesAllowingNewNullOrUndefined() {
+		filterPropertyAllowingAll: function extend_PropertyFilter_filterPropertyAllPropertiesAllowingNewNullOrUndefined(propertyName, targetObject, sourceObject) {
 			//accept all properties (including inherited ones) but disallow overwriting any existing properties unless they are null or undefined
 			return !targetObject.hasOwnProperty(propertyName) || targetObject[propertyName] === null || targetObject[propertyName] === undefined;
 	
@@ -107,7 +97,7 @@ this.extend(
 		
 		filterPropertyUsingNumber: function extend_PropertyFilter_filterPropertyUsingNumber(propertyName) {
 			//enumeration returns indices as strings
-			return propertyName === ""+this.filter;
+			return propertyName === String(parseFloat(this.filter));
 		},
 
 		filterPropertyUsingFunction: function extend_PropertyFilter_filterPropertyUsingFunction() {
@@ -120,7 +110,7 @@ this.extend(
 		},
 		
 		filterPropertyUsingArray: function extend_PropertyFilter_filterPropertyUsingRegExp(propertyName) {
-			return RegExp("^(" + this.filter.join("|") + ")$").test(propertyName);
+			return new RegExp("^(" + this.filter.join("|") + ")$").test(propertyName);
 		},
 		
 		filterPropertyUsingObject: function extend_PropertyFilter_filterPropertyUsingObject(propertyName) {
@@ -134,12 +124,8 @@ if (this.require) {
 	//register with requirejs if it's present
 	if (this.define) {
 		this.define(this.extend);
-	}
-	else if (this.require.def) {
+	} else if (this.require.def) {
 		this.require.def(this.extend);
 	}
 }
-
-//clean up any polluting global objects we've made
-this.extend.cleanReferences(this);
 
