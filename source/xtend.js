dd8f3f31597cba589e25b1d748ed0eaf7b220997
christianB <christian.bodart@thisisnumero.com>
2011-07-26 07:07:11 +0100

    Refactored extend to use itself! Still not actually tried any of the code...

--- source/xtend.js
+++ source/xtend.js
@@ -1,106 +1,112 @@
+if (typeof this.extend !== "undefined") {
+	//a property called extend has already been declared in global scope!
+	//store it and we will reassign later
+	this.extend_placeholder = this.extend;
+}
+
 //core function
-this.xtend = function xtend(targetObject, sourceObject, filter) {
+this.extend = function extend(targetObject, sourceObject, filter) {
 	//default is false when not passed
-	if (typeof filter === "undefined" || typeof filter === "null") {
-		filter = false; 
-	}
 	//convert the filter into a PropertyFilter if it is not already
-	if (filter.hasOwnProperty("filterProperty")) {
-		filter = new this.PropertyFilter(filter);
-	}
+	if (!filter || typeof filter.filterProperty !== "function") {
+		filter = new arguments.callee.PropertyFilter(filter);
+	} 
 	for (var propertyName in sourceObject) {
-		if (filter.filterProperty(propertyName, targetObject, sourceObject)
-	}
-};		
-//housekeeping method
-this.xtend.clean = function xtend_clean(scope) {
-
-	//cleans up any pollution caused by naming our functions by iterating and deleting the member
-	for (var propertyName in this.PropertyFilter.prototype) {
-		delete scope["xtend_PropertyFilter_" + propertyName];
-	}
-	
-	//delete the constructor for the PropertyFilter global scope
-	delete scope.xtend_PropertyFilter;
-
-	//voila should be clean now!!!! (apart from xtend that is!)
-};
-this.xtend.PropertyFilter = function xtend_PropertyFilter(filter) {
-	//actually passing a null value will perform a 'reset'
-	if (typeof filter !== "null") {
-		this.filter = filter;
-		this.filterProperty = this.resolvePropertyFilterMethod();
-	} else {//reset the object
-		this.reset();
-	}
-	if (this.constructor === Object) {
-	/*	
-		if we've been created from an object literal prototype (as we have done here)
-		fix the constructor property (without messing with the prototype, which is, of course, an Object)
-	*/
-		this.constructor = xtend_PropertyFilter || arguments.callee; //Sorry DC but it's not deprecated and you can't trust browsers (IE mostly) to handle scope correctly
-	}
-};
-this.xtend.PropertyFilter.prototype = {//assign our prototype here and use xtend to xtend its own helper class!
-	//sets the filter and reassigns the filterProperty method appropriately
-	setFilter: function xtend_PropertyFilter_setFilter(filter) {
-		this.constructor.call(this, filter);
-	},
-	//just a getter to match the setter!
-	getFilter: function xtend_PropertyFilter_getFilter() {
-		return this.filter;
-	},
-	//removes instance properties (effectively disposing the object)
-	reset: function xtend_PropertyFilter_reset() {
-		delete this.filter;
-		delete this.filterProperty;
-	},
-	//default filterProperty method - note it simply throws an exception - it should never be used
-	filterProperty: function xtend_PropertyFilter_filterProperty() {
-		throw "Illegal call to filterProperty on reset or misconfigured PropertyFilter (xtend.js)";
-	},
-	//this is the core 'logic' of the type - basically it decides which filterProperty method the instance should use
-	resolvePropertyFilterMethods: function xtend_PropertyFilter_resolvePropertyFilterMethods() {
-		switch (this.filter.constructor) {
-			case Boolean	: return this.filter ? this.allowAllPropertiesPassed : this.filterExistingProperties;
-			case Number		: //do the same as number
-			case String		: return this.filterPropertyUsingDirectComparison;
-			case Function	: return this.filterPropertyUsingFunction;
-			case RegExp		: return this.filterPropertyUsingRegularExpression;
-			case Array		: return this.convertFilterFromArrayToRegularExpression();//we call this function to change our filter to a regexp and return us an appropriate method
-			default			: return this.filterPropertyUsingObject;
+		if (filter.filterProperty(propertyName, targetObject, sourceObject)) {
+			targetObject[propertyName] = sourceObject[propertyName];
 		}
-	},
-
-	filterExistingProperties: function xtend_PropertyFilter_filterExistingProperties(propertyName, targetObject) {
-		//we disallow overwriting any existing properties unless they are null or undefined
-		return !targetObject.hasOwnProperty(propertyName) || targetObject[propertyName] === null || targetObject[propertyName] === undefined;
-	},
-
-	allowAllPropertiesPassed: function xtend_PropertyFilter_allowAllPropertiesPassed() {
-		return true;
-	},
-
-	convertFilterFromArrayToRegularExpression: function xtend_PropertyFilter_convertFilterFromArrayToRegularExpression() {
-		this.filter = new RegExp("^(" + this.filter.join("|") + ")$");
-		return this.filterPropertyUsingRegularExpression;//returns the method we want to use now
-	},
-
-	filterPropertyUsingDirectComparison: function xtend_PropertyFilter_filterPropertyUsingDirectComparison(propertyName) {
-		return propertyName === this.filter;
-	},
-
-	filterPropertyUsingFunction: function xtend_PropertyFilter_filterPropertyUsingFunction() {
-		//passes all three arguments straight through to the filterMethod
-		return this.filter.apply(null, arguments);
-	},
-	filterPropertyUsingRegularExpression: function xtend_PropertyFilter_filterPropertyUsingRegularExpression(propertyName) {
-		return this.filter.test(propertyName);
-	},
-
-	filterPropertyUsingObject: function xtend_PropertyFilter_filterPropertyUsingObject(propertyName) {
-		return typeof this.filter[propertyName] !== "undefined";
 	}
 };
+this.extend(
+	this.extend, {
+		//housekeeping method to remove any references the named functions leave in global scope;
+		cleanReferences: function xtend_cleanReferences(scope) {
+			//cleans up any pollution caused by naming our functions by iterating and deleting the member
+			for (var propertyName in this.PropertyFilter.prototype) {
+				delete scope["extend_PropertyFilter_" + propertyName];
+			}
+			//if we have used a placeholder property, clean up
+			if (scope.extend_placeholder) {
+				scope.extend = extend_placeholder
+				delete scope.extend_placeholder;
+			}
+			//delete the constructor for the PropertyFilter from global scope
+			delete scope.extend_PropertyFilter;
+			//voila should be clean now!!!! (apart from xtend that is!)
+		},
+		//extends core PropertyFilter object
+		PropertyFilter: function extend_PropertyFilter(filter) {
+			this.filter = filter||false;
+			//decide the strategy for filtering
+			this.filterProperty = this.resolvePropertyFilterMethod();
+		}
+};
+this.extend(
+	//extend the prototype of PropertyFilter type
+	this.extend.PropertyFilter.prototype, {
+	//Core implementation for PropertyInterface Implementation//	
+		//the only method 'required' to fulfill a FilterProperty 'implementation'
+		filterProperty: function extend_PropertyFilter_filterProperty() {
+			//defaut behaviour (occurs when no filter has been explicitly set in the object or the filter is set to false)
+			return this.filterPropertyAllowingNewNullOrUndefined.apply(this,arguments)
+		},
+		
+		//Properties
+		//default behaviour is to only copy new properties or those in the target set to null or undefined
+		filter: false,
+		
+		
+		//this is the core 'logic' of the type - basically it decides which filterProperty method the instance should use
+		resolvePropertyFilterMethod: function extend_PropertyFilter_resolvePropertyFilterMethods() {
+			switch (this.filter.constructor) {
+				case Boolean	: return this.filter ? this.filterPropertyAllowingAll : this.filterExistingPropertyAllowingNewNullOrUndefined;
+				case Number		: //do the same as number
+				case String		: return this.filterPropertyUsingString;
+				case Function	: return this.filterPropertyUsingFunction;
+				case RegExp		: return this.filterPropertyUsingRegExp;
+				case Array		: return this.filterPropertyUsingArray;
+				default			: return this.filterPropertyUsingObject;
+			}
+		},
+
+		filterPropertyAllowingNewNullOrUndefined: function extend_PropertyFilter_filterPropertyAllowingNewNullOrUndefined(propertyName, targetObject) {
+			//we disallow overwriting any existing properties unless they are null or undefined
+			return !targetObject.hasOwnProperty(propertyName) || targetObject[propertyName] === null || targetObject[propertyName] === undefined;
+		},
+
+		filterPropertyAllowingAll: function extend_PropertyFilter_filterPropertyAllowingAll() {
+			return true;
+		},
+		
+		filterPropertyUsingString: function extend_PropertyFilter_filterPropertyUsingString(propertyName) {
+			return propertyName === this.filter;
+		},
+		
+		filterPropertyUsingNumber: function extend_PropertyFilter_filterPropertyUsingNumber(propertyName) {
+			//enumeration returns indices as strings
+			return propertyName === ""+this.filter;
+		},
+
+		filterPropertyUsingFunction: function extend_PropertyFilter_filterPropertyUsingFunction() {
+			//passes all three arguments straight through to the filterMethod
+			return this.filter.apply(null, arguments);
+		},
+		filterPropertyUsingRegExp: function extend_PropertyFilter_filterPropertyUsingRegExp(propertyName) {
+			return this.filter.test(propertyName);
+		},
+		filterPropertyUsingArray: function extend_PropertyFilter_filterPropertyUsingRegExp(propertyName)
+			return RegExp("^(" + this.filter.join("|") + ")$").test(propertyName);
+		},
+		filterPropertyUsingObject: function extend_PropertyFilter_filterPropertyUsingObject(propertyName) {
+			return typeof this.filter[propertyName] !== "undefined";
+		}
+	}
+);
+if (this.require && this.define) {//
+	
+
+
+}
 //clean up any polluting global objects we've made
-this.xtend.clean(this);
+this.extend.cleanReferences(this);
+
