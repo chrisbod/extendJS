61c0740273000081586ac86846a4181eb1c1f07d
Christian Bodart <chris@bodar.com>
2011-08-01 19:39:06 +0100

    Getting augment up to spec

--- source/augment.js
+++ source/augment.js
@@ -1,69 +1,60 @@
-function augment(targetObject, sourceObject, filter, instancePropertyName, constructor, ConstructorToApply, forceRefresh) {
+function augment(targetObject, sourceObject, filter, initMethod) {
 	var callee = arguments.callee,
-		builtAugments = callee.builtAugments,
-		augmenter = instancePropertyName ? callee.getAugmenterBySourceAndPropertyName(sourceObject, instancePropertyName) : null;
-	if (!augmenter) {
-		augmenter = callee.createAugmenter(sourceObject, instancePropertyName, ConstructorToApply);
+		augmentor ;//= instancePropertyName ? callee.getAugmenterBySourceAndPropertyName(sourceObject, instancePropertyName) : null;
+	if (!augmentor) {
+		augmentor = callee.createAugmentor(sourceObject,filter,initMethod);
 	}
 	if (!targetObject.augmentations) {
-	targetObject.augmentations = [augmentor];
+		targetObject.augmentations = [augmentor];
 	}
 	else {
-		if (targetObject.hasOwnProperty("augmentations") {
-			targetObject.augmentations.push(augmenter);
+		if (targetObject.hasOwnProperty("augmentations")) {//probably a prototype or singleton / custom instance
+			targetObject.augmentations.push(augmentor);
 		}
 		else {
 			targetObject.augmentations = targetObject.augmentations.concat([augmentor]); //we make a duplicate of the 'inherited' collection and push this onto it
 		}
 	}
 	if (!targetObject.augment) {
-		targetObject.augment = this.augmentObject;
+		targetObject.augment = this.instance_augment;
 	}
-	return this.extend(targetObject, augmenter.augmentedProperties, filter);
+	return this.extend(targetObject, augmentor.propertyMap, filter);
 }
-augment.augmentObjectInstance = function augment_object_augment() {
-	for (var i = 0, augmentations = this.augmentations, l = augmentations.length, augmentation, Constructor = this.ConstructorToApply; i != l; i++) {
-		augmentation = augmentations[i];
-		if (augmentation.propertyName) {
-			this[propertyName] = new Constructor(this);
-		}
-		else {
-			Constructor.apply(this,[this])
-		}
+augment.instance_augment = function augment_object_augment() {
+	for (var i = 0, augmentations = this.augmentations, l = augmentations.length; i != l; i++) {
+		if (augmentations[i].initMethod) {
+			augmentation[i].initMethod.apply(this)
+		}	
 	}
-
 }
-augment.createAugmenter = function augment_createAugmenter (sourceObject, instancePropertyName) {
-	var augmenter = new this.Augmenter(sourceObject, instancePropertyName);
-	
+augment.createAugmentor = function augment_createAugmentor (sourceObject, initMethod) {
+	var id = null;//neeed to generate an id preferable meaningful (contructor name) - otherwise autgenerated
+	var augmentor = new this.Augmentor(id, sourceObject, initMethod);
+	augmentor.build();
+	return augmentor;
 };
-augment.Augmenter = function augment_Augmenter(sourceObject,propertyName,constructorToApply) {
+augment.Augmentor = function augment_Augmentor(id,sourceObject,initMethod) {
+	this.id = id;
 	this.sourceObject = sourceObject;
-	this.propertyName = propertyName;
-	this.constructorToApply = constructorToApply;
-	this.augmentedProperties = {};
+	this.initMethod = initMethod;
+	this.propertyMap = {}
 };
-augment.Augmenter.prototype = {
-	augmentMethod: function augment_Augmenter_augmentMethod(sourceObject, methodName) {
-	
-	//need to look for capitalized methods (constructors)
-		var augmentedMethod = this.augmentedProperties[methodName];
-		if (!augmentedMethod) {
-			//not using a closure - we want an unadultered method;
-			if (this.propertyName) {//we've been given a propertyName to point to so wrap it
-				augmentedMethod = this.augmentedProperties[methodName] = new Function("return this."+this.propertyName+"." + methodName + ".apply(" + this.propertyName + ", arguments)");
-			}
-			else {
-				augmentedMethod = sourceObject[methodName]; //map the method on directly
-			}
-
-			//END DEBUG
+augment.Augmentor.prototype = {
+	build: function () {
+	//index augmenter here - this is a full and raw copy - augmentations are consider to be interfaces and any inheritance in them should be included - the filter can be used to remove any unwanted properties if necessary
+		for (var propertyName in sourceObject) {
+			augmentor.propertyMap[i] = this.augmentValue(propertyName, sourceObject, sourceObject[i]);
 		}
-		return augmentedMethod;
 	},
-	augmentProperty : function (sourceObject, propertyName) {
-		//need to look for capitalized properties (constants)
-		this.augmentedProperties[propertyName] = sourceObject[propertyName];
+	augmentValue: function augment_Augmentor_augmentValue(propertyName, sourceObject, value) {
+		if (typeof method === "function") {
+			return this.augmentMethod(propertyName, sourceObject, value);
+		}
+		return value;
+	}
+	augmentMethod: function augment_Augmentor_augmentMethod(sourceObject, methodName) {
+		//need to look for capitalized methods (constructors||function constants)
+		return new Function("return this.augmentations."+this.id+"." + methodName + ".apply(this.augmentations."+this.id+", arguments)")
 	}
 };
 augment.counter = 0;
