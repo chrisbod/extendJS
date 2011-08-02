78f638c1749649d4729c6d8a7a0400a615cf3e6d
christianB <christian.bodart@thisisnumero.com>
2011-08-02 13:37:04 +0100

    exploring augment a little further

--- source/augment.js
+++ source/augment.js
@@ -1,8 +1,8 @@
-function augment(targetObject, sourceObject, filter, initMethodOrConstructor, nameOrDirect ) {//propertyNameForInstance will need implementing
+function augment(targetObject, sourceType, filter, augmentId ) {//propertyNameForInstance will need implementing
 	var callee = arguments.callee,
 		augmentor ;//= instancePropertyName ? callee.getAugmenterBySourceAndPropertyName(sourceObject, instancePropertyName) : null;
 	if (!augmentor) {
-		augmentor = callee.createAugmentor(sourceObject,filter,initMethodOrConstructor,nameOrDirect);
+		augmentor = callee.createAugmentor(sourceType,filter,augmentId);
 	}
 	if (!targetObject.augmentations) {
 		targetObject.augmentations = [augmentor];
@@ -21,56 +21,85 @@ function augment(targetObject, sourceObject, filter, initMethodOrConstructor, na
 	return this.extend(targetObject, augmentor.propertyMap, filter);
 }
 augment.instance_augment = function augment_object_augment() {
-	for (var i = 0, augmentations = this.augmentations, l = augmentations.length; i != l; i++) {
+	this.augmentations = this.augmentations.concat();//we need a new instance of the augmentations array with NO properties set - if this ever gets called on a prototype then weirdness will occur
+	for (var i = 0, augmentations = this.augmentations.concat(), l = augmentations.length; i != l; i++) {
 		augmentations[i].applyToInstance(this);
 	}
-}
-augment.createAugmentor = function augment_createAugmentor (sourceObject, initMethod, nameOrDirect) {
-	var id = null;//neeed to generate an id preferable meaningful (contructor name) - otherwise autgenerated
-	var augmentor = new this.Augmentor(id, sourceObject, initMethod,nameOrDirect);
+};
+augment.createAugmentor = function augment_createAugmentor (sourceType, augmentId) {
+	var applyDirect = false;
+	if (!augmentId && augmentId !== null) {
+		if (/function\s*([^\ \(]+)/.test(sourceType.toString())) {//we have a nice function name
+			augmentId = $2.replace(/^./, function (match) {
+				return match.toLowerCase();
+			});
+		}
+		else {
+			augmentId = this.generateAugmentorId();
+		}
+	} else {
+		applyDirect = true;
+		augmentId = this.generateAugmentorId();
+	}
+	var augmentor = new this.Augmentor(augmentId, sourceType, applyDirect);
 	augmentor.build();
 	return augmentor;
 };
-augment.Augmentor = function augment_Augmentor(id,sourceObject,initMethod) {
+augment.Augmentor = function augment_Augmentor(id, sourceType, applyDirect) {
 	this.id = id;
-	this.sourceObject = sourceObject;
-	this.initMethod = initMethod;
-	this.propertyMap = {}
+	this.applyDirect = applyDirect;
+	this.resolveType(sourceType);
+	this.propertyMap = {};
 };
 augment.Augmentor.prototype = {
+	resolveType: function (sourceType) {		
+		if (!this.applysourceType.Augmentor) {
+			this.sourceType = function augment_Augmentor() {
+				sourceType.Augmentor.apply(this);
+			};
+			this.sourceType.prototype = this.sourcePrototype;
+		} else {
+			this.sourceType = sourceType;
+		}
+	},
 	build: function () {
 	//index augmenter here - this is a full and raw copy - augmentations are consider to be interfaces and any inheritance in them should be included - the filter can be used to remove any unwanted properties if necessary
-		for (var propertyName in sourceObject) {
-			augmentor.propertyMap[i] = this.augmentValue(propertyName, sourceObject, sourceObject[i]);
+		for (var propertyName in this.sourcePrototype) {
+			augmentor.propertyMap[i] = this.augmentValue(propertyName, sourcePrototype, sourcePrototype[propertyName]);
 		}
 	},
 	applyToInstance: function (instance) {
-		if (this.sourceObject instanceof this.initMethod) {//passed a constructor
-			instance.augmentations[this.id] = new this.initMethod(instance);
+		if (this.applyDirect) {//apply directly to object
+			this.sourceType.apply(instance);
+		} else {
+			instance.augmentations[this.id] = new this.sourceType(instance);
 		}
-		else {
-			//apply the method to the instance
-			this.initMethod.apply(instance);
-		}
-	}
-	augmentValue: function augment_Augmentor_augmentValue(propertyName, sourceObject, value) {
+	},
+	augmentValue: function augment_Augmentor_augmentValue(propertyName, sourcePrototype, value) {
 		if (typeof method === "function") {
-			return this.augmentMethod(propertyName, sourceObject, value);
+			return this.augmentMethod(propertyName, sourcePrototype, value);
 		}
 		return value;
-	}
-	augmentMethod: function augment_Augmentor_augmentMethod(sourceObject, methodName) {
+	},
+	augmentMethod: function augment_Augmentor_augmentMethod(methodName,sourcePrototype,value) {
 		//need to look for capitalized methods (constructors||function constants)
-		return new Function("return this.augmentations."+this.id+"." + methodName + ".apply(this.augmentations."+this.id+", arguments)");
+		if (!this.applyDirect) {
+			return new Function("return this.augmentations."+this.id+"." + methodName + ".apply(this.augmentations."+this.id+", arguments)");
+		} else {
+			return new Function("return this.augmentations."+this.id+"." + methodName + ".apply(this, arguments)");
+		}
 		//
 	}
 };
 augment.counter = 0;
 augment.builtAugments = [];
+augment.generateAugmentorId = function () {
+	return "unnamedAugmentor"+this.counter++;
+}
 //Dummy Augmentation 'interface'
 augment.Augmentation = function augment_Augmentation(augmentee) {
 	this.augmentee = augmentee;
-}
+};
 augment.Augmentation.prototype = {
-	augmentee: void augment.Augmentation||Object,
-}
+	augmentee: void augment.Augmentation||Object
+};
